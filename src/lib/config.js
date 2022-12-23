5991890f22e1220181c0007f470cce2695741fe1
Thierno Dallo <thierno.diallo.oumar.mohamed@gmail.com>
2022-12-23 16:14:54 +0000

    Fix bug v3

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,11 +1,9 @@
-import { PUBLIC_FIREBASE_API_KEY, PUBLIC_AUTH_DOMAIN, PUBLIC_PROJECT_ID, PUBLIC_STRORAGE_BUCKET, PUBLIC_MESSAGING_SENDER_ID, PUBLIC_APP_ID } from '$env/static/public'
-
 // Your web app's Firebase configuration
 export const firebaseConfig = {
-  apiKey: PUBLIC_FIREBASE_API_KEY,
-  authDomain: PUBLIC_AUTH_DOMAIN,
-  projectId: PUBLIC_PROJECT_ID,
-  storageBucket: PUBLIC_STRORAGE_BUCKET,
-  messagingSenderId: PUBLIC_MESSAGING_SENDER_ID,
-  appId: PUBLIC_APP_ID,
-};
+    apiKey: "AIzaSyCKPm6tcAMO60bIzsoP5GivA831OW5kdJE",
+    authDomain: "nsia-389a7.firebaseapp.com",
+    projectId: "nsia-389a7",
+    storageBucket: "nsia-389a7.appspot.com",
+    messagingSenderId: "657542970344",
+    appId: "1:657542970344:web:5cfbf99f619472f4f3c25d",
+  };
